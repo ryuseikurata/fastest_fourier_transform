eeb59ce207dddb298e2b7157433d4f061dd11ebe
シュレック倉田 <32031454+ryuseikurata@users.noreply.github.com>
2021-06-06 20:13:39 +0900

    feat: foulier_transform

---
+++ src/basic_dft.rs
@@ -0,0 +1,6 @@
+use std::{marker::PhantomData, usize};
+
+pub fn foulier_transform(list: Vec<usize>) {
+    let length = list.len();
+    let list_1: Vec<usize> = Vec::with_capacity(length);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod basic_dft;
 fn main() {
     println!("Hello, world!");
 }
